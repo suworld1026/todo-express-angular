2ab10a407909a4efbab86fbfe91676dd854fa58e
Fabio Teixeira <fabio1079@gmail.com>
2016-03-08 10:12:43 -0300

    Add morgan module for log system
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- app.js
+++ app.js
@@ -2,15 +2,21 @@
 
 let express = require('express'),
     consign = require('consign'),
-    app = express();
+    morgan  = require('morgan'),
+    app     = express();
+
+/* Express configuration */
+app.use(morgan('tiny'));
 
 
+/* Autoload MVC structure */
 consign()
   .include('controllers')
   .then('routes')
   .into(app);
 
 
+/* Run development server */
 app.listen(3000, () => {
   console.log('Running on: http://127.0.0.1:3000');
 });
